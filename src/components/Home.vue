537343f2e938d45199353de584479d0392babc9f
Pablo Veiga <vcpablo@gmail.com>
2019-03-07 13:26:14 -0300

    feat(core): introduce new components

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <div  :class="$style.mainContainer">
+      <div :class="$style.message">
+      <h1 :class="$style.title">Thanks for Your Support</h1>
+      <p :class="$style.text">
+        Lorem ipsum dolor sivt amet, consectetur adipiscing elit. Donec non diam sit amet neque tristique aliquam. Curabitur non euismod felis. Suspendisse ligula sapien, fringilla non nunc sed, cursus dictum libero. Aliquam sit amet lorem vel enim posuere malesuada.
+      </p>
+    </div>
+    <app-form></app-form>
+    </div>
+    <div :class="$style.secondaryContainer">
+    </div>
+  </section>
+</template>
+
+<script>
+import Form from './Form'
+
+export default {
+  components: {
+    AppForm: Form
+  }
+}
+</script>
+
+<style module lang="scss">
+  
+</style>
